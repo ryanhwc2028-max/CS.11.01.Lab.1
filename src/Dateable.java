ec01859d8a4151d9bd7d8aa7f3605427c462c27b
ryanhwc2028-max <ryanhwc2028@student.cis.edu.hk>
2025-09-10 13:20:33 +0800

    adding a prompt

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int age, minimum;
         System.out.print("Enter your age: ");
-        age = 18;
+        age = scanner.nextInt();
         minimum = 7+(age/2);
         System.out.println(age + "-year olds should date somebody who is at least " + minimum + " years old");
     }   
